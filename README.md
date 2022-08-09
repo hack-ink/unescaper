703070524eac3f6a3278c13f5f9acc5ebb1608e4
Xavier Lau <xavier@inv.cafe>
2022-08-09 12:37:16 +0800

    Unescaper
    Signed-off-by: Xavier Lau <xavier@inv.cafe>

---
+++ README.md
@@ -0,0 +1,29 @@
+<div align="center">
+
+# Unescaper
+### Unescape strings with escape sequences written out as literal characters.
+
+[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Checks](https://github.com/hack-ink/unescaper/workflows/checks/badge.svg)](https://github.com/hack-ink/unescaper/actions/workflows/checks.yml)
+[![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/hack-ink/unescaper)](https://github.com/hack-ink/unescaper/tags)
+[![GitHub code lines](https://tokei.rs/b1/github/hack-ink/unescaper)](https://github.com/hack-ink/unescaper)
+[![GitHub last commit](https://img.shields.io/github/last-commit/hack-ink/unescaper?color=red&style=plastic)](https://github.com/hack-ink/unescaper)
+
+</div>
+
+## Usage
+[More Examples](src/test.rs)
+```rust
+fn main() {
+	assert_eq!(unescaper::unescape(r"\u000a").unwrap(), "\n");
+	assert_eq!(unescaper::unescape(r"\u{a}").unwrap(), "\n");
+	assert_eq!(unescaper::unescape(r"\x0a").unwrap(), "\n");
+	assert_eq!(unescaper::unescape(r"\12").unwrap(), "\n");
+}
+```
+
+## Thanks
+The idea comes from [unescape-rs](https://github.com/saghm/unescape-rs).<br>
+The last commit of that repository was seven years ago.<br>
+So, I think it is no longer maintained.<br>
+That's why I created this repository, and I have made some improvements.
---
+++ src/test.rs
@@ -0,0 +1,78 @@
+macro_rules! unescape_assert_eq {
+	($l:expr, $r:expr) => {
+		assert_eq!(crate::unescape($l).unwrap(), $r);
+	};
+}
+
+macro_rules! unescape_assert_err {
+	($l:expr, $r:expr) => {
+		assert_eq!(crate::unescape($l).unwrap_err(), $r);
+	};
+}
+
+macro_rules! unescape_assert_err_str {
+	($s:expr, $e:expr, $e_src:expr) => {{
+		let e = crate::unescape($s).unwrap_err();
+
+		assert_eq!(e.to_string(), $e);
+		assert_eq!(e.source().unwrap().to_string(), $e_src);
+	}};
+}
+
+#[test]
+fn error() {
+	// std
+	use std::error::Error;
+	// hack-ink
+	use crate::Error::*;
+
+	unescape_assert_err!(r"\", IncompleteStr(0));
+	unescape_assert_err!(r"\0\", IncompleteStr(2));
+
+	unescape_assert_err!(r"\{}", InvalidChar { char: '{', pos: 1 });
+	unescape_assert_err!(r"\0\{}", InvalidChar { char: '{', pos: 3 });
+
+	unescape_assert_err_str!(
+		r"\u{g}",
+		"parse int error, break at 4",
+		"invalid digit found in string"
+	);
+	unescape_assert_err_str!(
+		r"\0\u{g}",
+		"parse int error, break at 6",
+		"invalid digit found in string"
+	);
+}
+
+#[test]
+fn unescape_unicode() {
+	unescape_assert_eq!(r"\u0000", "\0");
+	unescape_assert_eq!(r"\u0009", "\t");
+	unescape_assert_eq!(r"\u000a", "\n");
+	unescape_assert_eq!(r"\uffff", "\u{ffff}");
+	unescape_assert_eq!(r"\u0000XavierJane", "\0XavierJane");
+
+	unescape_assert_eq!(r"\u{0}", "\0");
+	unescape_assert_eq!(r"\u{9}", "\t");
+	unescape_assert_eq!(r"\u{a}", "\n");
+	unescape_assert_eq!(r"\u{ffff}", "\u{ffff}");
+	unescape_assert_eq!(r"\u{0}XavierJane", "\0XavierJane");
+}
+
+#[test]
+fn unescape_byte() {
+	unescape_assert_eq!(r"\x00", "\x00");
+	unescape_assert_eq!(r"\x09", "\t");
+	unescape_assert_eq!(r"\x0a", "\n");
+	unescape_assert_eq!(r"\x7f", "\x7f");
+	unescape_assert_eq!(r"\x00XavierJane", "\x00XavierJane");
+}
+
+#[test]
+fn unescape_octal() {
+	unescape_assert_eq!(r"\0", "\0");
+	unescape_assert_eq!(r"\11", "\t");
+	unescape_assert_eq!(r"\12", "\n");
+	unescape_assert_eq!(r"\377", "\u{00ff}");
+	unescape_assert_eq!(r"\0XavierJane", "\0XavierJane");
+}
